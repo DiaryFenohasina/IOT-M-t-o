23c7c7475527421b0084f75fc6fcb95bd1dc0177
richartsdiaryfenohasina <richartsdiaryfenohasina@gmail.com>
2026-05-10 10:08:28 +0300

    feat/feat-mock-data

---
+++ Data/regionMock.ts
@@ -0,0 +1,18 @@
+export const data = {
+    tananarivo: {
+        latitude: -18.8792,
+        longitude: 47.5079
+    },
+    fianarantsoa: {
+        latitude: -21.4527,
+        longitude: 47.0857
+    },
+    diego: {
+        latitude: -12.2787,
+        longitude: 49.2917
+    },
+    toamasina: {
+        latitude: -18.1492,
+        longitude: 49.4023
+    }
+}
