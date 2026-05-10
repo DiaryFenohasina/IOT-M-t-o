a3a2343fb2084667e81e35246cba2b7f817a9fa7
richartsdiaryfenohasina <richartsdiaryfenohasina@gmail.com>
2026-05-10 11:02:33 +0300

    feat/feat-meteo-api

--- api/meteoApi.ts
+++ api/meteoApi.ts
@@ -1,19 +1,19 @@
-const switchLatLong = (lat: number = 52.52, long: number = 13.41) => {
-    return `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m`
-}
+import axios from "axios";
+import { APP_TIME_ZONE } from '@/utils/dateTime';
 
+export const getMeteoApi = (latitude : number, longitude : number) => {
+  return axios.create({
+    baseURL: `https://api.open-meteo.com/v1/forecast`,
+    timeout: 10000,
+    params: {
+      latitude,
+      longitude,
+      current: "temperature_2m,wind_speed_10m",
+      hourly:
+        "temperature_2m,relative_humidity_2m,wind_speed_10m",
+      timezone: APP_TIME_ZONE,
+    },
+  });
+};
 
-export const fetchWeatherData = async (lat: number = 52.52, long: number = 13.41) => {
-    try {
-        const response = await fetch(switchLatLong(lat, long));
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('Error fetching weather data:', error);
-        throw error;
-    }
-}
 
