7d31d33c88553b9eda47ce61fdd4ad0220fb231b
richartsdiaryfenohasina <richartsdiaryfenohasina@gmail.com>
2026-05-10 11:39:48 +0300

    feat/feat-province

--- data/regionMock.ts
+++ data/regionMock.ts
@@ -1,23 +1,38 @@
 export const regionData = {
   tananarivo: {
-    name: 'Tananarivo',
+    name: 'Antananarivo',
     latitude: -18.8792,
     longitude: 47.5079,
   },
+
   fianarantsoa: {
     name: 'Fianarantsoa',
     latitude: -21.4527,
     longitude: 47.0857,
   },
+
   diego: {
-    name: 'Diego',
+    name: 'Antsiranana',
     latitude: -12.2787,
     longitude: 49.2917,
   },
+
   toamasina: {
     name: 'Toamasina',
     latitude: -18.1492,
     longitude: 49.4023,
+  },
+
+  mahajanga: {
+    name: 'Mahajanga',
+    latitude: -15.7167,
+    longitude: 46.3167,
+  },
+
+  tulear: {
+    name: 'Toliara',
+    latitude: -23.3500,
+    longitude: 43.6667,
   },
 } as const;
 
